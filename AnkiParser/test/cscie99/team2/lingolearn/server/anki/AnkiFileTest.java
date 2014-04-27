243a221e767880d73d1a5f7c5771defa81f3a6c6
Jeff Rabe <7livesleft@gmail.com>
2014-04-27 18:40:08 -0400

    Added additional Tests to the AnkiFileTest class.

--- AnkiParser/test/cscie99/team2/lingolearn/server/anki/AnkiFileTest.java
+++ AnkiParser/test/cscie99/team2/lingolearn/server/anki/AnkiFileTest.java
@@ -1,10 +1,14 @@
 package cscie99.team2.lingolearn.server.anki;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Random;
 
 import junit.framework.TestCase;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import cscie99.team2.lingolearn.server.anki.error.AnkiException;
+import cscie99.team2.lingolearn.server.anki.models.CardModel;
 
 @RunWith(JUnit4.class)
 public class AnkiFileTest extends TestCase {
@@ -65,5 +70,54 @@ public class AnkiFileTest extends TestCase {
 	throws AnkiException
 	{
 		AnkiFile ankiFile = new AnkiFile(tmpFilename);
+	}
+	
+	/**
+	 * Test the case where an empty string is passed to the 
+	 * static AnkiFile.getModels() method.  This should return
+	 * a collection of zero models.
+	 * @throws JsonMappingException
+	 * @throws JsonParseException
+	 * @throws IOException
+	 */
+	@Test
+	public void ankiFile_noModelString_returnsNoModels()
+	throws JsonMappingException, JsonParseException, IOException
+	{
+		
+		HashMap<String, CardModel> models = AnkiFile.getModels("{ }");
+		
+		assertTrue( models.size() == 0 );
+	}
+	
+	/**
+	 * Test the case where a malformed Json string is passed to the
+	 * AnkiFile.getModels() method.  This should throw a JsonParseException
+	 * @throws JsonMappingException
+	 * @throws JsonParseException
+	 * @throws IOException
+	 */
+	@Test(expected=JsonParseException.class)
+	public void ankiFile_malformedJson_throwsException()
+	throws JsonMappingException, JsonParseException, IOException
+	{
+		String badJson = "stuff:{no end to this string";
+		HashMap<String, CardModel> models = AnkiFile.getModels(badJson);
+	}
+	
+	/**
+	 * Test the case that a String with no represented CardModels
+	 * is passed to the AnkiFile.getModels() method.  This should
+	 * throw an IOException (EofException).
+	 * @throws JsonMappingException
+	 * @throws JsonParseException
+	 * @throws IOException
+	 */
+	@Test(expected=IOException.class)
+	public void ankiFile_noJsonModels_throwsExcepton()
+	throws JsonMappingException, JsonParseException, IOException
+	{
+		String noModelJson = "{'one':'two', three:'four'}";
+		HashMap<String, CardModel> models = AnkiFile.getModels(noModelJson);
 	}
 }
